0efb4505cc294770259bfdde9eb94726be448f22
xMorningSt4rx <s4njikut3@gmail.com>
2024-09-19 15:38:13 +0700

    Chang Link API

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://capstonebackendwebapi-dvdbgtc4def9h3c2.japanwest-01.azurewebsites.net/";
+const url = "https://capstonewebapi-ejh5arauf8eqf3g7.japanwest-01.azurewebsites.net/";
 
 const api = axios.create({
     baseURL: url,
