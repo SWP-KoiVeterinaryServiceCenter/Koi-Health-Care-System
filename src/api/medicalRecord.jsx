53778358a8809883b6c6b0f455a3ff5e9d789cb3
xMorningSt4rx <s4njikut3@gmail.com>
2024-10-25 11:36:09 +0700

    CreateMedicalRecord

---
+++ src/api/medicalRecord.jsx
@@ -0,0 +1,18 @@
+import api from "./api";
+
+// export const getAllTanks = async () => {
+//     const response = await api.get(`/api/v1/Tank/GetAllTank`);
+//     return response.data;
+//   };
+
+export const createmedicalRecords = async (appointmentId, data) => {
+  const response = await api.post(
+    `/api/v1/MedicalRecord/CreateMedicalRecord/${appointmentId}`,
+    data
+  );
+  return response.data;
+};
+//   export const deleteTank= async (id ) => {
+//     const response = await api.delete(`/api/v1/Tank/RemoveTank/${id}`); //ban the user in account
+//     return response.data;
+//   };
---
+++ src/store/apiThunk/medicalRecord.jsx
@@ -0,0 +1,56 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import {
+    // getAllmedicalRecords,
+    createmedicalRecords,
+    // deletemedicalRecord
+  } from "../../api/medicalRecord";
+
+//   export const getAllmedicalRecordsThunk = createAsyncThunk(
+//     "medicalRecords/getAllmedicalRecords",
+//     async (thunkAPI) => {
+//       try {
+//         const response = await getAllmedicalRecords();
+//         return response;
+//       } catch (error) {
+//         return thunkAPI.rejectWithValue(error?.response?.data);
+//       }
+//     }
+//   );
+
+//   export const createmedicalRecordsThunk = createAsyncThunk(
+//     "medicalRecords/createmedicalRecords",
+//     async (data, thunkAPI) => {
+//         try {
+//             const response = await createmedicalRecords(data);
+//             return response;
+//         } catch (error) {
+//             return thunkAPI.rejectWithValue(error.response.data);
+            
+//         }
+//     }
+//   );
+export const createmedicalRecordsThunk = createAsyncThunk(
+    "medicalRecords/createmedicalRecords",
+    async ({ appointmentId, data }, thunkAPI) => {
+      try {
+        const response = await createmedicalRecords(appointmentId, data);
+        return response;
+      } catch (error) {
+        return thunkAPI.rejectWithValue(error.response?.data || 'Error');
+      }
+    }
+  );
+
+
+//   export const deletemedicalRecordThunk = createAsyncThunk(
+//     "medicalRecords/deletemedicalRecord",
+//     async (id, thunkAPI) => {
+//       try {
+//         const response = await deletemedicalRecord(id);
+//         return response;
+//       } catch (error) {
+//         return thunkAPI.rejectWithValue(error?.response?.data);
+//       }
+//     }
+//   );
+  
